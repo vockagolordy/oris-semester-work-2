f01cdf361ea783dfe7135f6b245ab6309bf3142c
ropoy <iramyruanova@gmail.com>
2025-12-18 22:06:56 +0300

    feat: add repository

--- src/main/java/ru/itis/scrabble/repositories/UserRepository.java
+++ src/main/java/ru/itis/scrabble/repositories/UserRepository.java
@@ -1,4 +1,24 @@
 package ru.itis.scrabble.repositories;
 
+import ru.itis.scrabble.models.User;
+
 public interface UserRepository {
+
+    User save(User user);
+
+    void update(User user);
+
+    void deleteById(Long id);
+
+    User findById(Long id);
+
+    User findByEmail(String email);
+
+    User findByUsername(String username);
+
+    boolean existsById(Long id);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
