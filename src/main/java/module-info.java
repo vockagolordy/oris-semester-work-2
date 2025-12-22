01c8d921be5ef7fb598d0ba7029f9c0ab104a771
ropoy <iramyruanova@gmail.com>
2025-12-22 22:21:25 +0300

    build: update artifacts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module ru.itis.scrabble {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
 
     opens ru.itis.scrabble to javafx.fxml;
