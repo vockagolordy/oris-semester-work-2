d1d655d66e0c05cda43e341059c0dcacbed1d5b1
ropoy <iramyruanova@gmail.com>
2025-12-22 22:17:55 +0300

    feat: update services

--- src/main/java/ru/itis/scrabble/services/ScoringServiceImpl.java
+++ src/main/java/ru/itis/scrabble/services/ScoringServiceImpl.java
@@ -1,4 +1,4 @@
-package ru.itis.scrabble.services.impl;
+package ru.itis.scrabble.services;
 
 import ru.itis.scrabble.dto.TilePlacementDTO;
 import ru.itis.scrabble.models.Board;
--- src/main/java/ru/itis/scrabble/services/SecurityService.java
+++
@@ -1,6 +0,0 @@
-package ru.itis.scrabble.services;
-
-public interface SecurityService {
-    void signupUser(String username, String password, String passwordRepeat);
-    void loginUser(String username, String password);
-}
