6700533b4227481057822ca4d6942fb8d5844b1a
ropoy <iramyruanova@gmail.com>
2025-12-24 11:37:16 +0300

    build: add dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ru.itis.scrabble {
     requires javafx.fxml;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires jakarta.persistence;
 
 
     opens ru.itis.scrabble to javafx.fxml;
